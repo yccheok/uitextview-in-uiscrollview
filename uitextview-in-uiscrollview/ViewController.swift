f3a58d1e6cb8db50d68c54ae24cca4bda9ea789c
yccheok <yancheng.cheok@gmail.com>
2021-01-08 16:12:02 +0800

    Initial creation.

---
+++ uitextview-in-uiscrollview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uitextview-in-uiscrollview
+//
+//  Created by Cheok Yan Cheng on 08/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
